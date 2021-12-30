ef6e0896862ca8a1a4d7653f7f5e35e00b21f600
kbys <work4kbys@gmail.com>
2021-12-30 16:11:53 +0900

    libpng fix

--- make-libpng.sh
+++ make-libpng.sh
@@ -3,16 +3,31 @@
 PREFIX=$(pwd)/build
 export PATH=${PREFIX}/bin:$PATH
 
+mkdir -p build/libpng-1.6.37-arm64
+mkdir -p build/libpng-1.6.37-x86_64
 (
   cd build
-  tar xf libpng-1.6.37.tar.gz
+  tar --strip-components=1 -xf libpng-1.6.37.tar.gz -C libpng-1.6.37-arm64
+  tar --strip-components=1 -xf libpng-1.6.37.tar.gz -C libpng-1.6.37-x86_64
 )
 
 mkdir -p build/lib/pkgconfig
 cp zlib.pc build/lib/pkgconfig/
 
 (
-  cd build/libpng-1.6.37
-  ./configure --prefix=${PREFIX} --enable-static=no CFLAGS="-g0 -O2 -arch arm64 -arch x86_64" LDFLAGS="-arch arm64 -arch x86_64"
+  cd build/libpng-1.6.37-x86_64
+  ./configure --prefix=${PREFIX} --enable-static=no CFLAGS="-g0 -O2 -arch x86_64" LDFLAGS="-arch x86_64"
   make install
 )
+
+mv build/lib/libpng16.16.dylib build/libpng16.16.x86_64.dylib
+
+(
+  cd build/libpng-1.6.37-arm64
+  ./configure --prefix=${PREFIX} --enable-static=no CFLAGS="-g0 -O2 -arch arm64" LDFLAGS="-arch arm64"
+  make install
+)
+
+mv build/lib/libpng16.16.dylib build/libpng16.16.arm64.dylib
+
+lipo -create build/libpng16.16.x86_64.dylib build/libpng16.16.arm64.dylib -output build/lib/libpng16.16.dylib
